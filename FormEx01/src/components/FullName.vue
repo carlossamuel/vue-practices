8d1d2594e0818426df73a852ddac417489368417
Carlos Cabral <carlos.samuel@gmail.com>
2019-01-04 11:01:14 +1100

    Dynamic Component, Form and WonderfullQuotes

---
+++ FormEx01/src/components/FullName.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="form-inline">
+    <div class="form-group">
+      <label for="first-name">First Name:</label>
+      <input type="text" class="form-control" id="first-name" v-model.lazy="firstName">
+    </div>
+    <div class="form-group">
+      <label for="last-name">Last name:</label>
+      <input type="text" class="form-control" id="last-name" v-model.lazy="lastName">
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+      props: {
+        value: {firstName:String, lastName:String},
+      },
+      data() {
+        return {
+         firstName: this.value.firstName,
+         lastName: this.value.lastName
+        }
+      },
+      watch: {
+        firstName (newValue) {
+          this.value.firstName = newValue;  
+          this.emitFullName();
+        },
+        lastName (newValue) {
+          this.value.lastName = newValue;  
+          this.emitFullName();
+        },
+      },
+      methods: {
+        emitFullName() {
+          this.$emit('input', this.value);
+        }
+      }     
+    }
+</script>
+
+<style scoped>    
+</style>
---
+++ FormEx01/src/components/Switch.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div
+                id="on"
+                @click="switched(trueValue)"
+                :class="{active: value == trueValue}">{{trueValue}}</div>
+        <div
+                id="off"
+                @click="switched(falseValue)"
+                :class="{active: value == falseValue}">{{falseValue}}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+      props: {
+        value: String,
+        trueValue: String,
+        falseValue: String
+      },
+      methods: {
+        switched (newValue) {
+          this.$emit('input', newValue);
+        }
+      }      
+    }
+</script>
+
+<style scoped>
+    #on, #off {
+        width: 40px;
+        height: 20px;
+        background-color: lightgray;
+        padding: 2px;
+        display: inline-block;
+        margin: 10px -2px;
+        box-sizing: content-box;
+        cursor: pointer;
+        text-align: center;
+    }
+
+    #on:hover, #on.active {
+        background-color: lightgreen;
+    }
+
+    #off:hover, #off.active {
+        background-color: lightcoral;
+    }
+</style>
